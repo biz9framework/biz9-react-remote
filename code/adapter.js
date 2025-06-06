6009ce7c3997c72f343dad2b12a7db49c681845d
Certified CoderZ <certifiedcoderz@gmail.com>
2025-06-05 21:59:12 -0400

    good

---
+++ code/adapter.js
@@ -0,0 +1,49 @@
+import axios  from 'axios';
+
+const get_data_adapter = (url) => {
+    return new Promise((callback) => {
+        axios.get(url)
+            .then(function (response) {
+                callback([response.data.cloud_error,response.data.cloud]);
+            })
+            .catch(function (error) {
+                console.log('Apdater-Get-Data-Error');
+                console.log(error);
+                callback(error);
+            });
+    })
+}
+
+const post_data_adapter = (url,obj) => {
+    return new Promise((callback) => {
+        axios.post(url,{data:obj}).then(function (response) {
+            callback([response.data.cloud_error,response.data.cloud]);
+        })
+            .catch(function (error) {
+                console.log('Apdater-Post-Data-Error');
+                console.log(error);
+                callback(error);
+            })
+
+    });
+}
+
+const delete_data_adapter = (url) => {
+    return new Promise((callback) => {
+        axios.delete(url).then(function (response) {
+            callback([response.data.cloud_error,response.data.cloud]);
+        })
+            .catch(function (error) {
+                console.log('Apdater-Delete-Data-Error');
+                console.log(error);
+                callback(error);
+            })
+
+    });
+}
+
+export {
+    get_data_adapter,
+    delete_data_adapter,
+    post_data_adapter
+}
