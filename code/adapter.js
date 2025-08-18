e5455bce4f4a351911e6bc9acd832df4bffbeeac
Certified CoderZ <certifiedcoderz@gmail.com>
2025-08-18 01:10:00 -0400

    good

--- code/adapter.js
+++ code/adapter.js
@@ -1,49 +1,32 @@
-alert('7777');
 import axios  from 'axios';
-// axios.get(url,{withCredentials:true})
-//axios.get(url)
 const get_data_adapter = (url) => {
     return new Promise((callback) => {
-    //axios.defaults.withCredentials = true;
-    //axios.get(url,{withCredentials:true})
-    axios.get(url)
-            .then(function (response) {
-                console.log('get_1_start');
-                console.log('response')
-                console.log(response);
-                console.log('get_1_end');
+    axios.defaults.withCredentials = true;
+    axios.get(url,{method:'GET',withCredentials:true}).then(function (response) {
                 callback([response.data.cloud_error,response.data.cloud]);
-            })
-            .catch(function (error) {
-                console.log('Apdater-Get-Data-Error');
-                console.log(error);
+            }).catch(function (error) {
                 callback([error.message,null]);
             });
     });
 }
 const post_data_adapter = (url,obj) => {
     return new Promise((callback) => {
-        axios.post(url,{data:obj},{withCredentials:true}).then(function (response) {
+        axios.defaults.withCredentials = true;
+        axios.post(url,{method:'POST',withCredentials:true}).then(function (response) {
             callback([response.data.cloud_error,response.data.cloud]);
-        })
-            .catch(function (error) {
-                console.log('Apdater-Post-Data-Error');
-                console.log(error);
+        }).catch(function (error) {
                 callback([error.message,null]);
-            })
-
+        })
     });
 }
 const delete_data_adapter = (url) => {
     return new Promise((callback) => {
-        axios.delete(url).then(function (response) {
+        axios.defaults.withCredentials = true;
+        axios.delete(url,{method:'DELETE',withCredentials:true}).then(function (response) {
             callback([response.data.cloud_error,response.data.cloud]);
-        })
-            .catch(function (error) {
-                console.log('Apdater-Delete-Data-Error');
-                console.log(error);
+        }).catch(function (error) {
                 callback([error.message,null]);
-            })
+        })
     });
 }
 export {
