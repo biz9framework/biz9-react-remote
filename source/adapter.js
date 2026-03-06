aeb40f48c0b8e6589762faa62739c57a501502d0
think1 <think1@localhost.localdomain>
2026-03-06 11:19:38 -0500

    good

--- source/adapter.js
+++ source/adapter.js
@@ -3,7 +3,7 @@ const get_data_adapter = (url) => {
     return new Promise((callback) => {
         axios.get(url)
             .then(function (response) {
-                callback([response.data.cloud_error,response.data.cloud_data]);
+                callback([response.data.error,response.data.data]);
             })
             .catch(function (error) {
                 console.log('Apdater-Get-Data-Error');
@@ -15,27 +15,25 @@ const get_data_adapter = (url) => {
 const post_data_adapter = (url,obj) => {
     return new Promise((callback) => {
         axios.post(url,obj).then(function (response) {
-            callback([response.data.cloud_error,response.data.cloud_data]);
+            callback([response.data.error,response.data.data]);
         })
             .catch(function (error) {
                 console.log('Apdater-Post-Data-Error');
                 console.log(error);
                 callback(error);
             })
-
     });
 }
 const delete_data_adapter = (url) => {
     return new Promise((callback) => {
         axios.delete(url).then(function (response) {
-            callback([response.data.cloud_error,response.data.cloud_data]);
+            callback([response.data.error,response.data.data]);
         })
             .catch(function (error) {
                 console.log('Apdater-Delete-Data-Error');
                 console.log(error);
                 callback(error);
             })
-
     });
 }
 export {
